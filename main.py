0907c4e5f003c7ef8f60de36de6f590285680f39
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-23 09:32:45 -0300

    Add images for each weekday in reminder messages

--- main.py
+++ main.py
@@ -35,6 +35,14 @@ times_do_lixo = {
     4: f"Time: <@&{roles['operacional']}>",
 }
 
+imagens_do_lixo = {
+    0: "https://scontent.fldb1-1.fna.fbcdn.net/v/t39.30808-6/500763396_1273374077677667_4743661265555671894_n.jpg?stp=dst-jpg_s720x720_tt6&_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=xN2ndkmh-F0Q7kNvwFrdr_D&_nc_oc=AdlISTXnmMCzzLtetNz0gukPLHqDGFTIDBJWCm_aO4cmRonXNS-B6P3X61kKsPq9Cuo&_nc_zt=23&_nc_ht=scontent.fldb1-1.fna&_nc_gid=uXKk0qcrgn5wSy-GtJMMLA&oh=00_AfYfXuwh1RNay8J8Xt2YYOXqE9FWRDN1EzAfLlv3z-9ivw&oe=68D84CC2",   # Segunda
+    1: "https://super.abril.com.br/wp-content/uploads/2017/03/preto-no-branco-a-explicac3a7c3a3o-cientc3adfica-para-as-cores-do-panda.png?crop=1&resize=1212,909",    # Terça
+    2: "https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2683012_0.webp?w=760&h=507",          # Quarta
+    3: "https://i.imgur.com/comercial.png",    # Quinta
+    4: "https://i.imgur.com/operacional.png",  # Sexta
+}
+
 async def enviar_lembrete():
     canal = bot.get_channel(CANAL_ID)
     hoje = datetime.datetime.now(TZ).weekday()
@@ -47,9 +55,13 @@ async def enviar_lembrete():
             ),
             color=discord.Color.green()
         )
-        embed.set_image(url="https://static.nationalgeographicbrasil.com/files/styles/image_3200/public/nationalgeographic2683012_0.webp?w=760&h=507")
+
+        # Só adiciona imagem se tiver para o dia
+        if hoje in imagens_do_lixo:
+            embed.set_image(url=imagens_do_lixo[hoje])
 
         await canal.send(embed=embed)
+
 
 @tasks.loop(minutes=1)
 async def checar_horario():
